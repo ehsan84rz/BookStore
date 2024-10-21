5c405ffcf12f30ec02bc0cbf1a8aa901e56e6c5d
ehsan <ehsan84rz@gmail.com>
2024-10-21 07:55:57 +0330

    Initial Commit

---
+++ books/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Book, Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'datetime_created', 'is_active', 'recommend', )
+
+
+admin.site.register(Book)
